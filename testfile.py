a945802a048df34bdf59c84f84e14ba93afa6816
Derek <derekdstratton@gmail.com>
2018-04-08 12:18:43 -0700

    file reading

--- testfile.py
+++ testfile.py
@@ -1,1 +1,12 @@
-print("hello")
+import csv
+
+with open("sampledata/trace.csv", "r") as f:
+    reader = csv.reader(f)
+    row = ['']
+    while row != ['EndHeader']:
+        row = reader.next()
+    #row now points at EndHeader, will be at first element after 1 next
+    #while True:
+    row = reader.next()
+    print(row[0])
+    print('hello')
